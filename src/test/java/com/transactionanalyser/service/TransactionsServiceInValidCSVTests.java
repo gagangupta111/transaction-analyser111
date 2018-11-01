80569e1430e5df5d00eb952f8bd0e54234e5a5a4
Gagan Gupta <gagan.gupta@AASPL-BRD.COM>
2018-11-01 16:34:16 +0530

    invalid reversal id exception

--- src/test/java/com/transactionanalyser/service/TransactionsServiceInValidCSVTests.java
+++ src/test/java/com/transactionanalyser/service/TransactionsServiceInValidCSVTests.java
@@ -14,6 +14,7 @@ import static com.transactionanalyser.constants.ValidatorErrorCodes.AMOUNT_IS_IN
 import static com.transactionanalyser.constants.ValidatorErrorCodes.DATE_IS_INVALID;
 import static com.transactionanalyser.constants.ValidatorErrorCodes.FILE_IS_INVALID;
 import static com.transactionanalyser.constants.ValidatorErrorCodes.MERCHANT_NAME_INVALID;
+import static com.transactionanalyser.constants.ValidatorErrorCodes.REVERSAL_ID_INVALID;
 import static com.transactionanalyser.constants.ValidatorErrorCodes.TYPE_IS_INVALID;
 import static com.transactionanalyser.validation.Validator.validateDate;
 
@@ -80,6 +81,17 @@ public class TransactionsServiceInValidCSVTests {
         expectedEx.expect(IllegalArgumentException.class);
         expectedEx.expectMessage(FILE_IS_INVALID);
         List<TransactionRecord> actualList = service.getTransactionsBaseOnParameters("xxx.csv",
+                validateDate("20/08/2018 12:00:00"),
+                validateDate("20/08/2018 13:00:00"),
+                "Kwik-E-Mart");
+    }
+
+    @Test
+    public void shouldThrowInvalidReversalIdsException() throws ParseException {
+
+        expectedEx.expect(IllegalArgumentException.class);
+        expectedEx.expectMessage(REVERSAL_ID_INVALID);
+        List<TransactionRecord> actualList = service.getTransactionsBaseOnParameters("invalid_reversal_ids_4.csv",
                 validateDate("20/08/2018 12:00:00"),
                 validateDate("20/08/2018 13:00:00"),
                 "Kwik-E-Mart");
